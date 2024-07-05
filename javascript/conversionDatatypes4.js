41df0121f6299035b3db4c78126383c09e887a8a
A-purwa <apurwawarathe1611@gmail.com>
2024-07-05 11:03:53 +0000

    created variables and understand different types of data and also worked on conversion of data

---
+++ javascript/conversionDatatypes4.js
@@ -0,0 +1,9 @@
+let num = 1234
+let valueInString = String(num)
+console.log(valueInString)
+console.log(typeof valueInString)
+
+let string = '123sss'
+let valueInNumber = Number(string)
+console.log(valueInNumber)                //output will be NAN 
+console.log(typeof valueInNumber)                 
---
+++ javascript/variable.js
@@ -0,0 +1,15 @@
+const accountId = 121 ;
+let emailAccount = 'apurwa@gmail.com';
+var password = '1234567'
+var accountCity = 'Indore'
+let userName ;
+//accountId = '789'
+emailAccount = 'bbc@gmail.com';
+password = '0987'
+accountCity= 'Bhopal'
+
+console.log(accountId)
+console.log([accountCity,password,emailAccount])
+console.table([userName,accountCity,password,emailAccount])
+
+/* prefer not to use var */
