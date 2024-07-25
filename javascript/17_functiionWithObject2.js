3fcee8f58bab3d91580385e40961d81e426e906f
A-purwa <apurwawarathe1611@gmail.com>
2024-07-25 12:49:47 +0000

    handle function with object

--- javascript/17_functiionWithObject2.js
+++ javascript/17_functiionWithObject2.js
@@ -1,5 +1,21 @@
 //spread operator or Rest operator it gives you the result in array
-function calculateCartPrice(price){
+function calculateCartPrice(...price){
     return price
 }
-console.log(calculateCartPrice(5))
+console.log(calculateCartPrice(5,55,555))
+
+function numbers(val1,val2,...num){
+    return num
+}
+console.log(numbers(100,200,300,400,500,600,700,800,900))
+
+//How to pass object in function
+
+const user = {
+    userName:'Apurwa',
+    place :'Sarni'
+}
+function handleObject(anyobject){
+  return   `my name is  ${anyobject.userName} and i live in ${anyobject.place}`
+}
+handleObject(user)
